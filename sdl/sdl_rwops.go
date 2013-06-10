7afd95471fbe9b7e0221b7801ac2d26c37096e4f
Jacky Boen <aqiank@gmail.com>
2013-06-10 23:43:02 +0800

    Made RWops more 'class-y'

--- sdl/sdl_rwops.go
+++ sdl/sdl_rwops.go
@@ -45,6 +45,9 @@ func RWFromFile(file, mode string) *RWops {
 }
 
 func RWFromMem(mem unsafe.Pointer, size int) *RWops {
+	if mem == nil {
+		return nil
+	}
 	_size := (C.int) (size)
 	return (*RWops) (unsafe.Pointer(C.SDL_RWFromMem(mem, _size)))
 }
@@ -53,119 +56,182 @@ func AllocRW() *RWops {
 	return (*RWops) (unsafe.Pointer(C.SDL_AllocRW()))
 }
 
-func FreeRw(area *RWops) {
+func (area *RWops) FreeRW() {
+	if area == nil {
+		return
+	}
 	_area := (*C.SDL_RWops) (area)
 	C.SDL_FreeRW(_area)
 }
 
-func RWsize(ctx *RWops) int64 {
+func (ctx *RWops) RWsize() int64 {
 	_ctx := (*C.SDL_RWops) (unsafe.Pointer(ctx))
 	return (int64) (C.RWsize(_ctx))
 }
 
-func RWseek(ctx *RWops, offset int64, whence int) int64 {
+func (ctx *RWops) RWseek(offset int64, whence int) int64 {
+	if ctx == nil {
+		return -1
+	}
 	_ctx := (*C.SDL_RWops) (unsafe.Pointer(ctx))
 	_offset := (C.Sint64) (offset)
 	_whence := (C.int) (whence)
 	return (int64) (C.RWseek(_ctx, _offset, _whence))
 }
 
-func RWread(ctx *RWops, ptr unsafe.Pointer, size, maxnum uint) uint {
+func (ctx *RWops) RWread(ptr unsafe.Pointer, size, maxnum uint) uint {
+	if ctx == nil {
+		return 0
+	}
 	_ctx := (*C.SDL_RWops) (unsafe.Pointer(ctx))
 	_size := (C.size_t) (size)
 	_maxnum := (C.size_t) (maxnum)
 	return (uint) (C.RWread(_ctx, ptr, _size, _maxnum))
 }
 
-func RWtell(ctx *RWops) int64 {
+func (ctx *RWops) RWtell() int64 {
+	if ctx == nil {
+		return 0
+	}
 	_ctx := (*C.SDL_RWops) (unsafe.Pointer(ctx))
 	return (int64) (C.RWseek(_ctx, 0, RW_SEEK_CUR))
 }
 
-func RWwrite(ctx *RWops, ptr unsafe.Pointer, size, num uint) uint {
+func (ctx *RWops) RWwrite(ptr unsafe.Pointer, size, num uint) uint {
+	if ctx == nil {
+		return 0
+	}
+	if ptr == nil {
+		return 0
+	}
 	_ctx := (*C.SDL_RWops) (unsafe.Pointer(ctx))
 	_size := (C.size_t) (size)
 	_num := (C.size_t) (num)
 	return (uint) (C.RWwrite(_ctx, ptr, _size, _num))
 }
 
-func RWclose(ctx *RWops) int {
+func (ctx *RWops) RWclose() int {
+	if ctx == nil {
+		return 0
+	}
 	_ctx := (*C.SDL_RWops) (unsafe.Pointer(ctx))
 	return (int) (C.RWclose(_ctx))
 }
 
-func ReadU8(src *RWops) uint8 {
+func (src *RWops) ReadU8() uint8 {
+	if src == nil {
+		return 0
+	}
 	_src := (*C.SDL_RWops) (unsafe.Pointer(src))
 	return (uint8) (C.SDL_ReadU8(_src))
 }
 
-func ReadLE16(src *RWops) uint16 {
+func (src *RWops) ReadLE16() uint16 {
+	if src == nil {
+		return 0
+	}
 	_src := (*C.SDL_RWops) (unsafe.Pointer(src))
 	return (uint16) (C.SDL_ReadLE16(_src))
 }
 
-func ReadBE16(src *RWops) uint16 {
+func (src *RWops) ReadBE16() uint16 {
+	if src == nil {
+		return 0
+	}
 	_src := (*C.SDL_RWops) (unsafe.Pointer(src))
 	return (uint16) (C.SDL_ReadBE16(_src))
 }
 
-func ReadLE32(src *RWops) uint32 {
+func (src *RWops) ReadLE32() uint32 {
+	if src == nil {
+		return 0
+	}
 	_src := (*C.SDL_RWops) (unsafe.Pointer(src))
 	return (uint32) (C.SDL_ReadLE32(_src))
 }
 
-func ReadBE32(src *RWops) uint32 {
+func (src *RWops) ReadBE32() uint32 {
+	if src == nil {
+		return 0
+	}
 	_src := (*C.SDL_RWops) (unsafe.Pointer(src))
 	return (uint32) (C.SDL_ReadBE32(_src))
 }
 
-func ReadLE64(src *RWops) uint64 {
+func (src *RWops) ReadLE64() uint64 {
+	if src == nil {
+		return 0
+	}
 	_src := (*C.SDL_RWops) (unsafe.Pointer(src))
 	return (uint64) (C.SDL_ReadLE64(_src))
 }
 
-func ReadBE64(src *RWops) uint64 {
+func (src *RWops) ReadBE64() uint64 {
+	if src == nil {
+		return 0
+	}
 	_src := (*C.SDL_RWops) (unsafe.Pointer(src))
 	return (uint64) (C.SDL_ReadBE64(_src))
 }
 
-func WriteU8(dst *RWops, value uint8) uint {
+func (dst *RWops) WriteU8(value uint8) uint {
+	if dst == nil {
+		return 0
+	}
 	_dst := (*C.SDL_RWops) (unsafe.Pointer(dst))
 	_value := (C.Uint8) (value)
 	return (uint) (C.SDL_WriteU8(_dst, _value))
 }
 
-func WriteLE16(dst *RWops, value uint16) uint {
+func (dst *RWops) WriteLE16(value uint16) uint {
+	if dst == nil {
+		return 0
+	}
 	_dst := (*C.SDL_RWops) (unsafe.Pointer(dst))
 	_value := (C.Uint16) (value)
 	return (uint) (C.SDL_WriteLE16(_dst, _value))
 }
 
-func WriteBE16(dst *RWops, value uint16) uint {
+func (dst *RWops) WriteBE16(value uint16) uint {
+	if dst == nil {
+		return 0
+	}
 	_dst := (*C.SDL_RWops) (unsafe.Pointer(dst))
 	_value := (C.Uint16) (value)
 	return (uint) (C.SDL_WriteBE16(_dst, _value))
 }
 
-func WriteLE32(dst *RWops, value uint32) uint {
+func (dst *RWops) WriteLE32(value uint32) uint {
+	if dst == nil {
+		return 0
+	}
 	_dst := (*C.SDL_RWops) (unsafe.Pointer(dst))
 	_value := (C.Uint32) (value)
 	return (uint) (C.SDL_WriteLE32(_dst, _value))
 }
 
-func WriteBE32(dst *RWops, value uint32) uint {
+func (dst *RWops) WriteBE32(value uint32) uint {
+	if dst == nil {
+		return 0
+	}
 	_dst := (*C.SDL_RWops) (unsafe.Pointer(dst))
 	_value := (C.Uint32) (value)
 	return (uint) (C.SDL_WriteBE32(_dst, _value))
 }
 
-func WriteLE64(dst *RWops, value uint64) uint {
+func (dst *RWops) WriteLE64(value uint64) uint {
+	if dst == nil {
+		return 0
+	}
 	_dst := (*C.SDL_RWops) (unsafe.Pointer(dst))
 	_value := (C.Uint64) (value)
 	return (uint) (C.SDL_WriteLE64(_dst, _value))
 }
 
-func WriteBE64(dst *RWops, value uint64) uint {
+func (dst *RWops) WriteBE64(value uint64) uint {
+	if dst == nil {
+		return 0
+	}
 	_dst := (*C.SDL_RWops) (unsafe.Pointer(dst))
 	_value := (C.Uint64) (value)
 	return (uint) (C.SDL_WriteBE64(_dst, _value))
