d44d1ba4e80002ab41b9fad3128fa2b542e36345
Jacky Boen <aqiank@gmail.com>
2013-06-11 18:38:59 +0800

    Added support for SDL_clipboard.h and SDL_cpuinfo.h

---
+++ sdl/sdl_clipboard.go
@@ -0,0 +1,19 @@
+package sdl
+
+// #include <SDL2/SDL_clipboard.h>
+import "C"
+import "unsafe"
+
+func SetClipboardText(text string) int {
+	_text := (C.CString) (text)
+	defer C.free(unsafe.Pointer(_text))
+	return (int) (C.SDL_SetClipboardText(_text))
+}
+
+func GetClipboardText() string {
+	return (C.GoString) (C.SDL_GetClipboardText())
+}
+
+func HasClipboardText() bool {
+	return C.SDL_HasClipboardText() > 0
+}
---
+++ sdl/sdl_cpuinfo.go
@@ -0,0 +1,48 @@
+package sdl
+
+// #include <SDL2/SDL.h>
+import "C"
+
+func GetCPUCount() int {
+	return (int) (C.SDL_GetCPUCount())
+}
+
+func GetCPUCacheLineSize() int {
+	return (int) (C.SDL_GetCPUCacheLineSize())
+}
+
+func HasRDTSC() bool {
+	return C.SDL_HasRDTSC() > 0
+}
+
+func HasAltiVec() bool {
+	return C.SDL_HasAltiVec() > 0
+}
+
+func HasMMX() bool {
+	return C.SDL_HasMMX() > 0
+}
+
+func Has3DNow() bool {
+	return C.SDL_Has3DNow() > 0
+}
+
+func HasSSE() bool {
+	return C.SDL_HasSSE() > 0
+}
+
+func HasSSE2() bool {
+	return C.SDL_HasSSE2() > 0
+}
+
+func HasSSE3() bool {
+	return C.SDL_HasSSE3() > 0
+}
+
+func HasSSE41() bool {
+	return C.SDL_HasSSE41() > 0
+}
+
+func HasSSE42() bool {
+	return C.SDL_HasSSE42() > 0
+}
