34e429eeb4d7909f33ae6dfb8fbd70599d8cdfa2
Jacky Boen <aqiank@gmail.com>
2013-09-28 17:23:12 +0800

    Fixed a bug in U8To32Array()
    Signed-off-by: Jacky Boen <aqiank@gmail.com>

--- sdl/helpers.go
+++ sdl/helpers.go
@@ -24,8 +24,8 @@ func Itob(i int) bool {
 func U8To32Array(buf []byte) []uint32 {
         var ret []uint32
         header := (*reflect.SliceHeader)(unsafe.Pointer(&ret))
-        header.Cap = len(ret) / 4 + 1
-        header.Len = len(ret) / 4 + 1
+        header.Cap = len(buf) / 4 + 1
+        header.Len = len(buf) / 4 + 1
         header.Data = uintptr(unsafe.Pointer(&buf[0]))
         return ret
 }
