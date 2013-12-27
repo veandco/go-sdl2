6a98ba1fc97ca33f67f82cf95c89c1bfc9c611fb
Jacky Boen <aqiank@gmail.com>
2013-12-28 02:30:15 +0800

    Added methods to access native windows
    Signed-off-by: Jacky Boen <aqiank@gmail.com>

--- sdl/sdl_syswm.go
+++ sdl/sdl_syswm.go
@@ -16,13 +16,56 @@ const (
 )
 
 type SysWMInfo struct {
-	Version	  Version
+	Version Version
 	Subsystem uint32
-	dummy	  [32]byte
+	dummy [24]byte
+}
+
+type WindowsInfo struct {
+	Window unsafe.Pointer
+}
+
+type X11Info struct {
+	Display unsafe.Pointer
+	Window uint
+}
+
+type DFBInfo struct {
+	Dfb unsafe.Pointer
+	Window unsafe.Pointer
+	Surface unsafe.Pointer
+}
+
+type CocoaInfo struct {
+	Window unsafe.Pointer
+}
+
+type UIKitInfo struct {
+	Window unsafe.Pointer
 }
 
 func (window *Window) GetWMInfo(info *SysWMInfo) bool {
 	_window := (*C.SDL_Window) (unsafe.Pointer(window))
 	_info := (*C.SDL_SysWMinfo) (unsafe.Pointer(info))
 	return C.SDL_GetWindowWMInfo(_window, _info) == 1
+}
+
+func (info *SysWMInfo) GetWindowsInfo() *WindowsInfo {
+	return (*WindowsInfo) (unsafe.Pointer(&info.dummy[0]));
+}
+
+func (info *SysWMInfo) GetX11Info() *X11Info {
+	return (*X11Info) (unsafe.Pointer(&info.dummy[0]));
+}
+
+func (info *SysWMInfo) GetDFBInfo() *DFBInfo {
+	return (*DFBInfo) (unsafe.Pointer(&info.dummy[0]));
+}
+
+func (info *SysWMInfo) GetCocoaInfo() *CocoaInfo {
+	return (*CocoaInfo) (unsafe.Pointer(&info.dummy[0]));
+}
+
+func (info *SysWMInfo) GetUIKitInfo() *UIKitInfo {
+	return (*UIKitInfo) (unsafe.Pointer(&info.dummy[0]));
 }
