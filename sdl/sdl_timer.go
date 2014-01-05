a17be80538b31c315a1a1cc03d5c9e389bec3dc9
Jacky Boen <aqiank@gmail.com>
2014-01-05 15:13:33 +0800

    Added SDL_GetTicks() and other functions in SDL_timer.h
    Signed-off-by: Jacky Boen <jackyb@localhost.localdomain>

--- sdl/sdl_timer.go
+++ sdl/sdl_timer.go
@@ -3,6 +3,18 @@ package sdl
 // #include <SDL2/SDL_timer.h>
 import "C"
 
+func GetTicks() uint32 {
+	return uint32(C.SDL_GetTicks())
+}
+
+func GetPerformanceCounter() uint64 {
+	return uint64(C.SDL_GetPerformanceCounter())
+}
+
+func GetPerformanceFrequency() uint64 {
+	return uint64(C.SDL_GetPerformanceFrequency())
+}
+
 func Delay(ms uint32) {
 	_ms := (C.Uint32) (ms)
 	C.SDL_Delay(C.Uint32(_ms));
