f676a3b7dc457b056c2fdb75e063a4dd227fdfab
Jeromy <jeromyj@gmail.com>
2014-02-03 21:31:12 -0800

    implement a lot more of the ttf library

--- sdl_ttf/sdl_ttf.go
+++ sdl_ttf/sdl_ttf.go
@@ -15,6 +15,10 @@ func Init() int {
 	return int(C.TTF_Init())
 }
 
+func WasInit() bool {
+	return int(C.TTF_WasInit()) != 0
+}
+
 func Quit() {
 	C.TTF_Quit()
 }
@@ -45,6 +49,36 @@ func (f *Font) RenderText_Solid(text string, color sdl.Color) *sdl.Surface {
 	return surface
 }
 
-func (f *Font) Close() {
-	C.TTF_CloseFont(f.f)
+func (f *Font) RenderText_Blended(text string, color sdl.Color) *sdl.Surface {
+	_text := C.CString(text)
+	_c := C.SDL_Color{C.Uint8(color.R), C.Uint8(color.G), C.Uint8(color.B), C.Uint8(color.A)}
+	surface := (*sdl.Surface) (unsafe.Pointer(C.TTF_RenderText_Blended(f.f, _text, _c)))
+	return surface
+}
+
+func (f *Font) RenderText_Shaded(text string, fg, bg sdl.Color) *sdl.Surface {
+	_text := C.CString(text)
+	_fg := C.SDL_Color{C.Uint8(fg.R), C.Uint8(fg.G), C.Uint8(fg.B), C.Uint8(fg.A)}
+	_bg := C.SDL_Color{C.Uint8(bg.R), C.Uint8(bg.G), C.Uint8(bg.B), C.Uint8(bg.A)}
+	surface := (*sdl.Surface) (unsafe.Pointer(C.TTF_RenderText_Shaded(f.f, _text, _fg, _bg)))
+	return surface
+}
+
+func (f *Font) Close() { C.TTF_CloseFont(f.f) }
+
+func (f *Font) Height() int { return int(C.TTF_FontHeight(f.f)) }
+func (f *Font) Ascent() int { return int(C.TTF_FontAscent(f.f)) }
+func (f *Font) Descent() int { return int(C.TTF_FontDescent(f.f)) }
+func (f *Font) LineSkip() int { return int(C.TTF_FontLineSkip(f.f)) }
+func (f *Font) Faces() int { return int(C.TTF_FontFaces(f.f)) }
+
+func (f *Font) FaceIsFixedWidth() bool {
+	return int(C.TTF_FontFaceIsFixedWidth(f.f)) != 0
+}
+
+func (f *Font) FaceFamilyName() string {
+	_fname := C.TTF_FontFaceFamilyName(f.f)
+	fname := C.GoString(_fname)
+	C.free(unsafe.Pointer(_fname))
+	return fname
 }
