22d593c6bce23b4d68313be41088ab76826ac7db
Marcus von Appen <mva@sysfault.org>
2014-04-21 09:24:09 +0200

    - do not use SDL_free() on C.CString() items that were allocated by go's own   memory management routines, since SDL_malloc() and SDL_free() might use an   own memory manager (dlmalloc), if malloc() is not available on the target   platform, the SDL2 library was built for

--- sdl/sdl_clipboard.go
+++ sdl/sdl_clipboard.go
@@ -7,7 +7,7 @@ import "unsafe"
 
 func SetClipboardText(text string) int {
 	_text := C.CString(text)
-	defer C.SDL_free(unsafe.Pointer(_text))
+	defer C.free(unsafe.Pointer(_text))
 	return (int)(C.SDL_SetClipboardText(_text))
 }
 
--- sdl/sdl_loadso.go
+++ sdl/sdl_loadso.go
@@ -6,13 +6,13 @@ import "unsafe"
 
 func LoadObject(sofile string) unsafe.Pointer {
 	_sofile := C.CString(sofile)
-	defer C.SDL_free(unsafe.Pointer(_sofile))
+	defer C.free(unsafe.Pointer(_sofile))
 	return (unsafe.Pointer)(C.SDL_LoadObject(_sofile))
 }
 
 func LoadFunction(handle unsafe.Pointer, name string) unsafe.Pointer {
 	_name := C.CString(name)
-	defer C.SDL_free(unsafe.Pointer(_name))
+	defer C.free(unsafe.Pointer(_name))
 	return (unsafe.Pointer)(C.SDL_LoadFunction(handle, _name))
 }
 
