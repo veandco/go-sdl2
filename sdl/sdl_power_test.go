ffb6f762d8c3e3c135fddeb905bfa88af4b7428d
Marcus von Appen <marcus@sysfault.org>
2014-04-25 17:32:05 +0200

    - Added wrapper for  SDL_GetPowerInfo()

---
+++ sdl/sdl_power_test.go
@@ -0,0 +1,25 @@
+package sdl
+
+import "testing"
+
+func TestGetPowerInfo(t *testing.T) {
+	state, seconds, percent := GetPowerInfo()
+	strstate := "Unknown"
+	minutes := -1
+	if state == POWERSTATE_CHARGED {
+		strstate = "Battery charged"
+	} else if state == POWERSTATE_CHARGING {
+		strstate = "Battery charging"
+	} else if state == POWERSTATE_NO_BATTERY {
+		strstate = "No battery"
+	} else if state == POWERSTATE_ON_BATTERY   {
+		strstate = "On battery"
+	}
+	if seconds != -1 {
+		minutes = seconds / 60
+	}
+	t.Log("Power status report, please check manually")
+	t.Logf("Status: %s", strstate)
+	t.Logf("Minutes left (-1 = undetermined): %d", minutes)
+	t.Logf("Percent left (-1 = undetermined): %d", percent)
+}
