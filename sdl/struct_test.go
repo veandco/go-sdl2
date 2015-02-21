a9703609d27c006f58d7fb2755c6f7070c40cb5d
Philipp Meinen <philipp@bind.ch>
2015-02-21 14:01:49 +0100

    cleanup struct abi tests

--- sdl/struct_test.go
+++ sdl/struct_test.go
@@ -6,55 +6,54 @@ import (
 	"testing"
 )
 
-// TODO: AudioCVT is a packed struct in C - proper conversion needs some more work
-// func TestStructAudioCVT(t *testing.T)  { testStructEq(t, AudioCVT{}, cAudioCVT{}) }
-func TestStructAudioSpec(t *testing.T)   { testStructEq(t, AudioSpec{}, cAudioSpec{}) }
-func TestStructDisplayMode(t *testing.T) { testStructEq(t, DisplayMode{}, cDisplayMode{}) }
+func TestStructABI(t *testing.T) {
+	var tests = []struct {
+		gStruct interface{}
+		cStruct interface{}
+	}{
+		{AudioSpec{}, cAudioSpec{}},
+		// TODO: AudioCVT is a packed struct in C - proper conversion needs some more work
+		//{ AudioCVT{}, cAudioCVT{} },
+		{DisplayMode{}, cDisplayMode{}},
 
-// EVENTS
+		// EVENTS
+		{KeyDownEvent{}, cKeyboardEvent{}},
+		{KeyUpEvent{}, cKeyboardEvent{}},
+		{MouseButtonEvent{}, cMouseButtonEvent{}},
+		{MouseMotionEvent{}, cMouseMotionEvent{}},
+		{MouseWheelEvent{}, cMouseWheelEvent{}},
+		{TextEditingEvent{}, cTextEditingEvent{}},
+		{TextInputEvent{}, cTextInputEvent{}},
+		{JoyAxisEvent{}, cJoyAxisEvent{}},
+		{JoyBallEvent{}, cJoyBallEvent{}},
+		{JoyHatEvent{}, cJoyHatEvent{}},
+		{JoyButtonEvent{}, cJoyButtonEvent{}},
+		{ControllerAxisEvent{}, cControllerAxisEvent{}},
+		{ControllerButtonEvent{}, cControllerButtonEvent{}},
+		{ControllerDeviceEvent{}, cControllerDeviceEvent{}},
+		{TouchFingerEvent{}, cTouchFingerEvent{}},
+		{MultiGestureEvent{}, cMultiGestureEvent{}},
+		{DollarGestureEvent{}, cDollarGestureEvent{}},
+		{DropEvent{}, cDropEvent{}},
+		{UserEvent{}, cUserEvent{}},
+		{SysWMEvent{}, cSysWMEvent{}},
 
-func TestStructKeyDown(t *testing.T)     { testStructEq(t, KeyDownEvent{}, cKeyboardEvent{}) }
-func TestStructKeyUp(t *testing.T)       { testStructEq(t, KeyUpEvent{}, cKeyboardEvent{}) }
-func TestStructMouseButton(t *testing.T) { testStructEq(t, MouseButtonEvent{}, cMouseButtonEvent{}) }
-func TestStructMouseMotion(t *testing.T) { testStructEq(t, MouseMotionEvent{}, cMouseMotionEvent{}) }
-func TestStructMouseWheel(t *testing.T)  { testStructEq(t, MouseWheelEvent{}, cMouseWheelEvent{}) }
-func TestStructTextEditing(t *testing.T) { testStructEq(t, TextEditingEvent{}, cTextEditingEvent{}) }
-func TestStructTextInput(t *testing.T)   { testStructEq(t, TextInputEvent{}, cTextInputEvent{}) }
-func TestStructJoyAxis(t *testing.T)     { testStructEq(t, JoyAxisEvent{}, cJoyAxisEvent{}) }
-func TestStructJoyBall(t *testing.T)     { testStructEq(t, JoyBallEvent{}, cJoyBallEvent{}) }
-func TestStructJoyHat(t *testing.T)      { testStructEq(t, JoyHatEvent{}, cJoyHatEvent{}) }
-func TestStructJoyButton(t *testing.T)   { testStructEq(t, JoyButtonEvent{}, cJoyButtonEvent{}) }
-func TestStructControllerAxis(t *testing.T) {
-	testStructEq(t, ControllerAxisEvent{}, cControllerAxisEvent{})
+		{Palette{}, cPalette{}},
+		{PixelFormat{}, cPixelFormat{}},
+		{Surface{}, cSurface{}},
+		{Version{}, cVersion{}},
+		{WindowEvent{}, cWindowEvent{}},
+
+		// TODO: embedded structures make testing difficult
+		// { SysWMInfo{}, cSysWMinfo{} },
+		// { RendererInfo{}, realcRendererInfo{} },
+	}
+
+	for _, test := range tests {
+		testStructABI(t, test.gStruct, test.cStruct)
+	}
 }
-func TestStructControllerBtn(t *testing.T) {
-	testStructEq(t, ControllerButtonEvent{}, cControllerButtonEvent{})
-}
-func TestStructControllerDev(t *testing.T) {
-	testStructEq(t, ControllerDeviceEvent{}, cControllerDeviceEvent{})
-}
-func TestStructTouchFinger(t *testing.T)  { testStructEq(t, TouchFingerEvent{}, cTouchFingerEvent{}) }
-func TestStructMultiGesture(t *testing.T) { testStructEq(t, MultiGestureEvent{}, cMultiGestureEvent{}) }
-func TestStructDollarGesture(t *testing.T) {
-	testStructEq(t, DollarGestureEvent{}, cDollarGestureEvent{})
-}
-func TestStructDrop(t *testing.T)  { testStructEq(t, DropEvent{}, cDropEvent{}) }
-func TestStructUser(t *testing.T)  { testStructEq(t, UserEvent{}, cUserEvent{}) }
-func TestStructSysWM(t *testing.T) { testStructEq(t, SysWMEvent{}, cSysWMEvent{}) }
-
-// END OF EVENTS
-
-func TestStructPalette(t *testing.T)     { testStructEq(t, Palette{}, cPalette{}) }
-func TestStructPixelFormat(t *testing.T) { testStructEq(t, PixelFormat{}, cPixelFormat{}) }
-func TestStructSurface(t *testing.T)     { testStructEq(t, Surface{}, cSurface{}) }
-func TestStructVersion(t *testing.T)     { testStructEq(t, Version{}, cVersion{}) }
-func TestStructWindowEvent(t *testing.T) { testStructEq(t, WindowEvent{}, cWindowEvent{}) }
-
-// TODO: embedded structures make testing difficult
-// func TestStructSysWmInfo(t *testing.T)   { testStructEq(t, SysWMInfo{}, cSysWMinfo{}) }
-// func TestStructRendererInfo(t *testing.T) {	testStructEq(t, RendererInfo{}, realcRendererInfo{})}
-
-func testStructEq(t *testing.T, a, b interface{}) {
+func testStructABI(t *testing.T, a, b interface{}) {
 	ta, tb := reflect.TypeOf(a), reflect.TypeOf(b)
 	if ta.Size() != tb.Size() {
 		t.Errorf("struct size missmatch: %s(%d) != %s(%d)",
