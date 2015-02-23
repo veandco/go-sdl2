3d637c4b30abf3aaaf57ee851e2918078d4ac819
jalan <jalanpalmer@gmail.com>
2015-02-23 13:18:36 -0500

    Make LowerBlit idiomatic with respect to errors

--- sdl/surface.go
+++ sdl/surface.go
@@ -260,8 +260,11 @@ func (src *Surface) UpperBlit(srcRect *Rect, dst *Surface, dstRect *Rect) error 
 }
 
 // Surface (https://wiki.libsdl.org/SDL_LowerBlit)
-func (src *Surface) LowerBlit(srcRect *Rect, dst *Surface, dstRect *Rect) int {
-	return int(C.SDL_LowerBlit(src.cptr(), srcRect.cptr(), dst.cptr(), dstRect.cptr()))
+func (src *Surface) LowerBlit(srcRect *Rect, dst *Surface, dstRect *Rect) error {
+	if C.SDL_LowerBlit(src.cptr(), srcRect.cptr(), dst.cptr(), dstRect.cptr()) != 0 {
+		return GetError()
+	}
+	return nil
 }
 
 // Surface (https://wiki.libsdl.org/SDL_SoftStretch)
