82e1c9db5dc13c51d9e54fd8869e209e43a0fa41
Brandon Mulcahy <brandon@jangler.info>
2015-06-10 23:59:38 -0400

    Run go fmt on sdl_ttf package

--- sdl_ttf/sdl_ttf.go
+++ sdl_ttf/sdl_ttf.go
@@ -124,7 +124,7 @@ func OpenFontIndexRW(src *sdl.RWops, freesrc, size, index int) (*Font, error) {
 }
 
 // RenderUTF8_Solid (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_44.html#SEC44)
-func (f *Font) RenderUTF8_Solid(text string, color sdl.Color) (*sdl.Surface,error) {
+func (f *Font) RenderUTF8_Solid(text string, color sdl.Color) (*sdl.Surface, error) {
 	_text := C.CString(text)
 	defer C.free(unsafe.Pointer(_text))
 	_c := C.SDL_Color{C.Uint8(color.R), C.Uint8(color.G), C.Uint8(color.B), C.Uint8(color.A)}
@@ -136,7 +136,7 @@ func (f *Font) RenderUTF8_Solid(text string, color sdl.Color) (*sdl.Surface,erro
 }
 
 // RenderUTF8_Shaded (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_48.html#SEC48)
-func (f *Font) RenderUTF8_Shaded(text string, fg, bg sdl.Color) (*sdl.Surface,error) {
+func (f *Font) RenderUTF8_Shaded(text string, fg, bg sdl.Color) (*sdl.Surface, error) {
 	_text := C.CString(text)
 	defer C.free(unsafe.Pointer(_text))
 	_fg := C.SDL_Color{C.Uint8(fg.R), C.Uint8(fg.G), C.Uint8(fg.B), C.Uint8(fg.A)}
@@ -149,7 +149,7 @@ func (f *Font) RenderUTF8_Shaded(text string, fg, bg sdl.Color) (*sdl.Surface,er
 }
 
 // RenderUTF8_Blended (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_52.html#SEC52)
-func (f *Font) RenderUTF8_Blended(text string, color sdl.Color) (*sdl.Surface,error) {
+func (f *Font) RenderUTF8_Blended(text string, color sdl.Color) (*sdl.Surface, error) {
 	_text := C.CString(text)
 	defer C.free(unsafe.Pointer(_text))
 	_c := C.SDL_Color{C.Uint8(color.R), C.Uint8(color.G), C.Uint8(color.B), C.Uint8(color.A)}
@@ -160,7 +160,7 @@ func (f *Font) RenderUTF8_Blended(text string, color sdl.Color) (*sdl.Surface,er
 	return surface, nil
 }
 
-func (f *Font) RenderUTF8_Blended_Wrapped(text string, fg sdl.Color,wrapLength int) (*sdl.Surface,error) {
+func (f *Font) RenderUTF8_Blended_Wrapped(text string, fg sdl.Color, wrapLength int) (*sdl.Surface, error) {
 	_text := C.CString(text)
 	defer C.free(unsafe.Pointer(_text))
 	_c := C.SDL_Color{C.Uint8(fg.R), C.Uint8(fg.G), C.Uint8(fg.B), C.Uint8(fg.A)}
@@ -171,20 +171,18 @@ func (f *Font) RenderUTF8_Blended_Wrapped(text string, fg sdl.Color,wrapLength i
 	return surface, nil
 }
 
-
 // SizeUTF8 (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_40.html#SEC40)
-func (f *Font) SizeUTF8(text string) (int,int,error) {
+func (f *Font) SizeUTF8(text string) (int, int, error) {
 	_text := C.CString(text)
 	defer C.free(unsafe.Pointer(_text))
 	var w C.int
 	var h C.int
-	result :=  C.TTF_SizeUTF8(f.f,_text,&w,&h)
+	result := C.TTF_SizeUTF8(f.f, _text, &w, &h)
 	if result == 0 {
-		return int(w),int(h),nil
+		return int(w), int(h), nil
 	}
-	return int(w),int(h),GetError()
+	return int(w), int(h), GetError()
 }
-
 
 // Close (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_18.html#SEC18)
 func (f *Font) Close() {
@@ -193,19 +191,19 @@ func (f *Font) Close() {
 }
 
 // Height (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_29.html#SEC29)
-func (f *Font) Height() int   { return int(C.TTF_FontHeight(f.f)) }
+func (f *Font) Height() int { return int(C.TTF_FontHeight(f.f)) }
 
 // Ascent (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_30.html#SEC30)
-func (f *Font) Ascent() int   { return int(C.TTF_FontAscent(f.f)) }
+func (f *Font) Ascent() int { return int(C.TTF_FontAscent(f.f)) }
 
 // Descent (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_31.html#SEC31)
-func (f *Font) Descent() int  { return int(C.TTF_FontDescent(f.f)) }
+func (f *Font) Descent() int { return int(C.TTF_FontDescent(f.f)) }
 
 // LineSkip (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_32.html#SEC32)
 func (f *Font) LineSkip() int { return int(C.TTF_FontLineSkip(f.f)) }
 
 // Faces (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_33.html#SEC33)
-func (f *Font) Faces() int    { return int(C.TTF_FontFaces(f.f)) }
+func (f *Font) Faces() int { return int(C.TTF_FontFaces(f.f)) }
 
 // GetStyle (https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_21.html#SEC21)
 func (f *Font) GetStyle() int {
