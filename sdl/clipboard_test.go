e5ae38c18b7000a027a2f59f0bdf3adbf778e882
Brandon Mulcahy <brandon@jangler.info>
2015-07-24 16:40:02 -0400

    Return errors from clipboard functions, test

---
+++ sdl/clipboard_test.go
@@ -0,0 +1,29 @@
+package sdl
+
+import "testing"
+
+func TestClipboard(t *testing.T) {
+	Init(INIT_VIDEO)
+	defer Quit()
+
+	// need a window to own the clipboard
+	if win, err := CreateWindow("", 0, 0, 0, 0, 0); err == nil {
+		defer win.Destroy()
+	} else {
+		t.Skipf("Could not CreateWindow(): %v", err)
+	}
+
+	wantString, wantBool := []string{"", "naïveté"}, []bool{false, true}
+	for i := 0; i < 2; i++ {
+		if err := SetClipboardText(wantString[i]); err != nil {
+			t.Skipf("Could not SetClipboardText(): %v", err)
+		}
+		if got := HasClipboardText(); got != wantBool[i] {
+			t.Errorf("HasClipboardText() == %v; want %v", got, wantBool[i])
+		}
+		if got, err := GetClipboardText(); err == nil && got != wantString[i] {
+			t.Errorf("GetClipboardText() == (nil, %#v); want (nil, %#v)",
+				got, wantString[i])
+		}
+	}
+}
