53f37c3284043911bb129a689e861ddd53744a26
Charney Kaye <charney@outright.io>
2015-12-31 22:01:54 -0500

    On Linux, ensure references to SDL2 headers

--- sdl/sdl_wrapper.h
+++ sdl/sdl_wrapper.h
@@ -1,6 +1,8 @@
 #if defined(__WIN32)
 	#include <SDL2/SDL.h>
 	#include <stdlib.h>
+#elif defined(__linux__)
+	#include <SDL2/SDL.h>
 #else
 	#include <SDL.h>
 #endif
--- sdl_image/sdl_image_wrapper.h
+++ sdl_image/sdl_image_wrapper.h
@@ -1,6 +1,8 @@
 #if defined(__WIN32)
 	#include <SDL2/SDL_image.h>
 	#include <stdlib.h>
+#elif defined(__linux__)
+	#include <SDL2/SDL_image.h>
 #else
 	#include <SDL_image.h>
 #endif
