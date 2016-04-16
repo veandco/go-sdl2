2291fe20f1d341cc6666056473d585872515bd80
Jacky Boen <aqiank@gmail.com>
2016-04-16 22:52:59 +0800

    Add some print calls to see queue and goroutine execution order
    Signed-off-by: Jacky Boen <aqiank@gmail.com>

--- examples/render_queue/render_queue.go
+++ examples/render_queue/render_queue.go
@@ -31,60 +31,105 @@ func run() int {
 	sdl.CallQueue <- func() {
         renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
         if err != nil {
-            fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
+            fmt.Fprint(os.Stderr, "Failed to create renderer: %s\n", err)
             os.Exit(2)
         }
         renderer.Clear()
+        println("queue: A")
     }
     defer renderer.Destroy()
+
+    go func() {
+        println("goroutine: A")
+    }()
 
     sdl.CallQueue <- func() {
         renderer.SetDrawColor(255, 255, 255, 255)
         renderer.DrawPoint(150, 300)
+        println("queue: B")
     }
+
+    go func() {
+        println("goroutine: B")
+    }()
 
     sdl.CallQueue <- func() {
         renderer.SetDrawColor(0, 0, 255, 255)
         renderer.DrawLine(0, 0, 200, 200)
+        println("queue: C")
     }
+
+    go func() {
+        println("goroutine: C")
+    }()
 
     sdl.CallQueue <- func() {
         points = []sdl.Point{{0, 0}, {100, 300}, {100, 300}, {200, 0}}
         renderer.SetDrawColor(255, 255, 0, 255)
         renderer.DrawLines(points)
+        println("queue: D")
     }
+
+    go func() {
+        println("goroutine: D")
+    }()
 
     sdl.CallQueue <- func() {
         rect = sdl.Rect{300, 0, 200, 200}
         renderer.SetDrawColor(255, 0, 0, 255)
         renderer.DrawRect(&rect)
+        println("queue: E")
     }
+
+    go func() {
+        println("goroutine: E")
+    }()
 
     sdl.CallQueue <- func() {
         rects = []sdl.Rect{{400, 400, 100, 100}, {550, 350, 200, 200}}
         renderer.SetDrawColor(0, 255, 255, 255)
         renderer.DrawRects(rects)
+        println("queue: F")
     }
+
+    go func() {
+        println("goroutine: F")
+    }()
 
     sdl.CallQueue <- func() {
         rect = sdl.Rect{250, 250, 200, 200}
         renderer.SetDrawColor(0, 255, 0, 255)
         renderer.FillRect(&rect)
+        println("queue: G")
     }
+
+    go func() {
+        println("goroutine: G")
+    }()
 
     sdl.CallQueue <- func() {
         rects = []sdl.Rect{{500, 300, 100, 100}, {200, 300, 200, 200}}
         renderer.SetDrawColor(255, 0, 255, 255)
         renderer.FillRects(rects)
+        println("queue: H")
     }
+
+    go func() {
+        println("goroutine: H")
+    }()
 
     sdl.CallQueue <- func() {
         renderer.Present()
+        println("queue: I")
     }
 
-	sdl.Delay(2000)
+    go func() {
+        println("goroutine: I")
+    }()
 
-	return 0
+    sdl.Delay(2000)
+
+    return 0
 }
 
 func main() {
