7f6dc5dd50eebe414b196dbb0176874a98151510
Lilis Iskandar <veeableful@gmail.com>
2016-08-04 02:44:05 +0800

    examples: gfx: updated example
    Signed-off-by: Lilis Iskandar <veeableful@gmail.com>

--- examples/gfx/gfx.go
+++ examples/gfx/gfx.go
@@ -16,6 +16,7 @@ var winWidth, winHeight int = 800, 600
 func run() int {
 	var window *sdl.Window
 	var renderer *sdl.Renderer
+	var vx, vy = make([]int16, 3), make([]int16, 3)
 	var wg sync.WaitGroup // Set a WaitGroup to wait until all pixels are drawn
 	var err error
 
@@ -48,6 +49,18 @@ func run() int {
 
 	// Wait until all pixels are drawn
 	wg.Wait()
+
+	vx[0] = int16(winWidth / 3)
+	vy[0] = int16(winHeight / 3)
+	vx[1] = int16(winWidth * 2 / 3)
+	vy[1] = int16(winHeight / 3)
+	vx[2] = int16(winWidth / 2)
+	vy[2] = int16(winHeight * 2 / 3)
+	gfx.FilledPolygonColor(renderer, vx, vy, 0xFF0000FF)
+
+	gfx.CharacterColor(renderer, winWidth - 16, 16, 'X', 0xFFFF0000)
+	gfx.StringColor(renderer, 16, 16, "GFX Demo", 0xFF00FF00)
+
 	renderer.Present()
 	sdl.Delay(3000)
 
