6714be1993d86c199f628d09710b9c532c36ddd8
Lilis Iskandar <veeableful@gmail.com>
2016-08-04 22:10:01 +0800

    added experimental sdl_gfx
    Signed-off-by: Lilis Iskandar <veeableful@gmail.com>

--- sdl_gfx/sdl_gfx.go
+++ sdl_gfx/sdl_gfx.go
@@ -8,7 +8,6 @@ package gfx
 import "C"
 import "github.com/veandco/go-sdl2/sdl"
 import "unsafe"
-//import "errors"
 
 // FPS
 const (
@@ -17,7 +16,57 @@ const (
 	FPS_DEFAULT     = 30
 )
 
-type FPSManager C.FPSmanager
+type FPSmanager struct {
+	FrameCount uint32
+	RateTicks  float32
+	BaseTicks  uint32
+	LastTicks  uint32
+	Rate       uint32
+}
+
+const (
+	SMOOTHING_OFF = 0
+	SMOOTHING_ON  = 0
+)
+
+func min(a ...int) int {
+	b := a[0]
+
+	for i := 1; i < len(a); i++ {
+		if b < a[0] {
+			b = a[0]
+		}
+	}
+
+	return b
+}
+
+func (manager *FPSmanager) cptr() *C.FPSmanager {
+	return (*C.FPSmanager)(unsafe.Pointer(manager))
+}
+
+func InitFramerate(manager *FPSmanager) {
+	C.SDL_initFramerate(manager.cptr())
+}
+
+func SetFramerate(manager *FPSmanager, rate uint32) bool {
+	_rate := C.Uint32(rate)
+	return C.SDL_setFramerate(manager.cptr(), _rate) == 0
+}
+
+func GetFramerate(manager *FPSmanager) (int, bool) {
+	fps := int(C.SDL_getFramerate(manager.cptr()))
+	return fps, fps >= 0
+}
+
+func GetFramecount(manager *FPSmanager) (int, bool) {
+	count := int(C.SDL_getFramerate(manager.cptr()))
+	return count, count >= 0
+}
+
+func FramerateDelay(manager *FPSmanager) uint32 {
+	return uint32(C.SDL_framerateDelay(manager.cptr()))
+}
 
 func PixelColor(renderer *sdl.Renderer, x, y int, color uint32) bool {
 	_x := C.Sint16(x)
@@ -469,99 +518,57 @@ func FilledTrigonRGBA(renderer *sdl.Renderer, x1, y1, x2, y2, x3, y3 int, r, g, 
 func PolygonColor(renderer *sdl.Renderer, vx, vy []int16, color uint32) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_color := C.Uint32(color)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.polygonColor(renderer, _vx, _vy, _len, _color) == 0
 }
 
 func PolygonRGBA(renderer *sdl.Renderer, vx, vy []int16, r, g, b, a uint8) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_r := C.Uint8(r)
 	_g := C.Uint8(g)
 	_b := C.Uint8(b)
 	_a := C.Uint8(a)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.polygonRGBA(renderer, _vx, _vy, _len, _r, _g, _b, _a) == 0
 }
 
 func AAPolygonColor(renderer *sdl.Renderer, vx, vy []int16, color uint32) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_color := C.Uint32(color)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.aapolygonColor(renderer, _vx, _vy, _len, _color) == 0
 }
 
 func AAPolygonRGBA(renderer *sdl.Renderer, vx, vy []int16, r, g, b, a uint8) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_r := C.Uint8(r)
 	_g := C.Uint8(g)
 	_b := C.Uint8(b)
 	_a := C.Uint8(a)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.aapolygonRGBA(renderer, _vx, _vy, _len, _r, _g, _b, _a) == 0
 }
 
 func FilledPolygonColor(renderer *sdl.Renderer, vx, vy []int16, color uint32) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_color := C.Uint32(color)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.filledPolygonColor(renderer, _vx, _vy, _len, _color) == 0
 }
 
 func FilledPolygonRGBA(renderer *sdl.Renderer, vx, vy []int16, r, g, b, a uint8) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_r := C.Uint8(r)
 	_g := C.Uint8(g)
 	_b := C.Uint8(b)
 	_a := C.Uint8(a)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.filledPolygonRGBA(renderer, _vx, _vy, _len, _r, _g, _b, _a) == 0
 }
 
@@ -569,51 +576,30 @@ func TexturedPolygon(renderer *sdl.Renderer, vx, vy []int16, surface *sdl.Surfac
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
 	_surface := (*C.SDL_Surface)(unsafe.Pointer(surface))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_textureDX := C.int(textureDX)
 	_textureDY := C.int(textureDY)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.texturedPolygon(renderer, _vx, _vy, _len, _surface, _textureDX, _textureDY) == 0
 }
 
 func BezierColor(renderer *sdl.Renderer, vx, vy []int16, s int, color uint32) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_s := C.int(s)
 	_color := C.Uint32(color)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.bezierColor(renderer, _vx, _vy, _len, _s, _color) == 0
 }
 
 func BezierRGBA(renderer *sdl.Renderer, vx, vy []int16, s int, r, g, b, a uint8) bool {
 	_vx := (*C.Sint16)(unsafe.Pointer(&vx[0]))
 	_vy := (*C.Sint16)(unsafe.Pointer(&vy[0]))
-	_len := C.int(0)
+	_len := C.int(min(len(vx), len(vy)))
 	_s := C.int(s)
 	_r := C.Uint8(r)
 	_g := C.Uint8(g)
 	_b := C.Uint8(b)
 	_a := C.Uint8(a)
-
-	if len(vx) < len(vy) {
-		_len = C.int(len(vx))
-	} else {
-		_len = C.int(len(vy))
-	}
-
 	return C.bezierRGBA(renderer, _vx, _vy, _len, _s, _r, _g, _b, _a) == 0
 }
 
@@ -665,4 +651,272 @@ func StringRGBA(renderer *sdl.Renderer, x, y int, s string, r, g, b, a uint8) bo
 	_b := C.Uint8(b)
 	_a := C.Uint8(a)
 	return C.stringRGBA(renderer, _x, _y, _s, _r, _g, _b, _a) == 0
+}
+
+func ImageFilterMMXdetect() bool {
+	return C.SDL_imageFilterMMXdetect() > 0
+}
+
+func ImageFilterMMXoff() {
+	C.SDL_imageFilterMMXoff()
+}
+
+func ImageFilterMMXon() {
+	C.SDL_imageFilterMMXon()
+}
+
+func ImageFilterAdd(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterAdd(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterMean(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterMean(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterSub(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterSub(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterAbsDiff(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterAbsDiff(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterMult(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterMult(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterMultNor(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterMultNor(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterMultDivby2(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterMultDivby2(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterMultDivby4(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterMultDivby4(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterBitAnd(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterBitAnd(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterBitOr(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterBitOr(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterDiv(src1, src2, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_src2 := (*C.uchar)(unsafe.Pointer(&src2[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(src2), len(dest)))
+	return C.SDL_imageFilterDiv(_src1, _src2, _dest, _len) == 0
+}
+
+func ImageFilterBitNegation(src1, dest []byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	return C.SDL_imageFilterBitNegation(_src1, _dest, _len) == 0
+}
+
+func ImageFilterAddByte(src1, dest []byte, c byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_c := C.uchar(c)
+	return C.SDL_imageFilterAddByte(_src1, _dest, _len, _c) == 0
+}
+
+func ImageFilterAddUint(src1, dest []byte, c uint) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_c := C.uint(c)
+	return C.SDL_imageFilterAddUint(_src1, _dest, _len, _c) == 0
+}
+
+func ImageFilterSubByte(src1, dest []byte, c byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_c := C.uchar(c)
+	return C.SDL_imageFilterSubByte(_src1, _dest, _len, _c) == 0
+}
+
+func ImageFilterSubUint(src1, dest []byte, c uint) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_c := C.uint(c)
+	return C.SDL_imageFilterSubUint(_src1, _dest, _len, _c) == 0
+}
+
+func ImageFilterShiftRight(src1, dest []byte, n byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_n := C.uchar(n)
+	return C.SDL_imageFilterShiftRight(_src1, _dest, _len, _n) == 0
+}
+
+func ImageFilterShiftRightUint(src1, dest []byte, n byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_n := C.uchar(n)
+	return C.SDL_imageFilterShiftRightUint(_src1, _dest, _len, _n) == 0
+}
+
+func ImageFilterMultByByte(src1, dest []byte, c byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_c := C.uchar(c)
+	return C.SDL_imageFilterMultByByte(_src1, _dest, _len, _c) == 0
+}
+
+func ImageFilterShiftRightAndMultByByte(src1, dest []byte, n, c byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_n := C.uchar(n)
+	_c := C.uchar(c)
+	return C.SDL_imageFilterShiftRightAndMultByByte(_src1, _dest, _len, _n, _c) == 0
+}
+
+func ImageFilterShiftLeftByte(src1, dest []byte, n byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_n := C.uchar(n)
+	return C.SDL_imageFilterShiftLeftByte(_src1, _dest, _len, _n) == 0
+}
+
+func ImageFilterShiftLeftUint(src1, dest []byte, n byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_n := C.uchar(n)
+	return C.SDL_imageFilterShiftLeftUint(_src1, _dest, _len, _n) == 0
+}
+
+func ImageFilterShiftLeft(src1, dest []byte, n byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_n := C.uchar(n)
+	return C.SDL_imageFilterShiftLeft(_src1, _dest, _len, _n) == 0
+}
+
+func ImageFilterBinarizeUsingThreshold(src1, dest []byte, t byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_t := C.uchar(t)
+	return C.SDL_imageFilterBinarizeUsingThreshold(_src1, _dest, _len, _t) == 0
+}
+
+func ImageFilterClipToRange(src1, dest []byte, tmin, tmax byte) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_tmin := C.uchar(tmin)
+	_tmax := C.uchar(tmax)
+	return C.SDL_imageFilterClipToRange(_src1, _dest, _len, _tmin, _tmax) == 0
+}
+
+func ImageFilterNormalizeLinear(src1, dest []byte, cmin, cmax, nmin, nmax int) bool {
+	_src1 := (*C.uchar)(unsafe.Pointer(&src1[0]))
+	_dest := (*C.uchar)(unsafe.Pointer(&dest[0]))
+	_len := C.uint(min(len(src1), len(dest)))
+	_cmin := C.int(cmin)
+	_cmax := C.int(cmax)
+	_nmin := C.int(nmin)
+	_nmax := C.int(nmax)
+	return C.SDL_imageFilterNormalizeLinear(_src1, _dest, _len, _cmin, _cmax, _nmin, _nmax) == 0
+}
+
+func RotoZoomSurface(src *sdl.Surface, angle, zoom float64, smooth int) *sdl.Surface {
+	_angle := C.double(angle)
+	_zoom := C.double(zoom)
+	_smooth := C.int(smooth)
+	return (*sdl.Surface)(unsafe.Pointer(C.rotozoomSurface(unsafe.Pointer(src), _angle, _zoom, _smooth)))
+}
+
+func RotoZoomSurfaceXY(src *sdl.Surface, angle, zoomx, zoomy float64, smooth int) *sdl.Surface {
+	_angle := C.double(angle)
+	_zoomx := C.double(zoomx)
+	_zoomy := C.double(zoomy)
+	_smooth := C.int(smooth)
+	return (*sdl.Surface)(unsafe.Pointer(C.rotozoomSurfaceXY(unsafe.Pointer(src), _angle, _zoomx, _zoomy, _smooth)))
+}
+
+func ZoomSurface(src *sdl.Surface, zoomx, zoomy float64, smooth int) *sdl.Surface {
+	_zoomx := C.double(zoomx)
+	_zoomy := C.double(zoomy)
+	_smooth := C.int(smooth)
+	return (*sdl.Surface)(unsafe.Pointer(C.zoomSurface(unsafe.Pointer(src), _zoomx, _zoomy, _smooth)))
+}
+
+func ZoomSurfaceSize(width, height int, zoomx, zoomy float64) (dstwidth int, dstheight int) {
+	_width := C.int(width)
+	_height := C.int(height)
+	_zoomx := C.double(zoomx)
+	_zoomy := C.double(zoomy)
+	_dstwidth := (*C.int)(unsafe.Pointer(&dstwidth))
+	_dstheight := (*C.int)(unsafe.Pointer(&dstheight))
+	C.zoomSurfaceSize(_width, _height, _zoomx, _zoomy, _dstwidth, _dstheight)
+	return dstwidth, dstheight
+}
+
+func ShrinkSurface(src *sdl.Surface, factorx, factory int) *sdl.Surface {
+	_factorx := C.int(factorx)
+	_factory := C.int(factory)
+	return (*sdl.Surface)(unsafe.Pointer(C.shrinkSurface(unsafe.Pointer(src), _factorx, _factory)))
+}
+
+func RotateSurface90Degrees(src *sdl.Surface, numClockwiseTurns int) *sdl.Surface {
+	_numClockwiseTurns := C.int(numClockwiseTurns)
+	return (*sdl.Surface)(unsafe.Pointer(C.rotateSurface90Degrees(unsafe.Pointer(src), _numClockwiseTurns)))
 }
--- sdl_gfx/sdl_gfx_wrapper.h
+++ sdl_gfx/sdl_gfx_wrapper.h
@@ -1,8 +1,12 @@
 #if defined(__WIN32)
 	#include <SDL2/SDL2_framerate.h>
 	#include <SDL2/SDL2_gfxPrimitives.h>
+	#include <SDL2/SDL2_imageFilter.h>
+	#include <SDL2/SDL2_rotozoom.h>
 	#include <stdlib.h>
 #else
 	#include <SDL2_framerate.h>
 	#include <SDL2_gfxPrimitives.h>
+	#include <SDL2_imageFilter.h>
+	#include <SDL2_rotozoom.h>
 #endif
