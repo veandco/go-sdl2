7b1ac14372fb372f8b02f7caca0dd619fc58f286
Julien C <julien.castelain@liferay.com>
2017-02-03 11:33:39 +0100

    Add comment for Color.RGBA

--- sdl/pixels.go
+++ sdl/pixels.go
@@ -54,6 +54,13 @@ func (c Color) Uint32() uint32 {
 	return v
 }
 
+// RGBA returns the alpha-premultiplied red, green, blue and alpha values
+// for the color. Each value ranges within [0, 0xffff], but is represented
+// by a uint32 so that multiplying by a blend factor up to 0xffff will not
+// overflow.
+//
+// An alpha-premultiplied color component c has been scaled by alpha (a),
+// so has valid values 0 <= c <= a.
 func (c Color) RGBA(r, g, b, a uint32) {
 	r = uint32(c.R)
 	r |= r << 8
