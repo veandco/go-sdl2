3060b61b8d2f9b36f21dd813de611b42e1f8f539
Julien C <julien.castelain@liferay.com>
2017-02-03 11:29:58 +0100

    Implement standard library Color interface

--- sdl/pixels.go
+++ sdl/pixels.go
@@ -54,6 +54,18 @@ func (c Color) Uint32() uint32 {
 	return v
 }
 
+func (c Color) RGBA(r, g, b, a uint32) {
+	r = uint32(c.R)
+	r |= r << 8
+	g = uint32(c.G)
+	g |= g << 8
+	b = uint32(c.B)
+	b |= b << 8
+	a = uint32(c.A)
+	a |= a << 8
+	return
+}
+
 const (
 	PIXELTYPE_UNKNOWN  = C.SDL_PIXELTYPE_UNKNOWN
 	PIXELTYPE_INDEX1   = C.SDL_PIXELTYPE_INDEX1
