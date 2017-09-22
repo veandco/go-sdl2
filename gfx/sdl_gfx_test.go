ac5c42f4de6bc7fd177048a8fdab64ef1345e14a
Paul Glass <pnglass@gmail.com>
2017-09-21 20:52:39 -0500

    gfx: Correct GetFramecount call (#258)
    - Update `gfx.GetFramecount` to correctly call `SDL_getFramecount`
    - Add `gfx` to the makefile package list (so gfx tests are run)
    - Add tests for the FPSmanager

--- gfx/sdl_gfx_test.go
+++ gfx/sdl_gfx_test.go
@@ -13,3 +13,29 @@ func TestMin(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestFPSmanager(t *testing.T) {
+	fpsManager := FPSmanager{}
+	InitFramerate(&fpsManager)
+
+	if !SetFramerate(&fpsManager, 60) {
+		t.Errorf("gfx.SetFramerate failed")
+	}
+
+	if fps, ok := GetFramerate(&fpsManager); !ok || fps != 60 {
+		t.Errorf("gfx.GetFramerate() = (%v, %v) - want (60, true)", fps, ok)
+	}
+
+	// frame count is initialized to 0
+	if count, ok := GetFramecount(&fpsManager); !ok || count != 0 {
+		t.Errorf("gfx.GetFramecount() = (%v, %v) - want (0, true)", count, ok)
+	}
+
+	// count one frame
+	FramerateDelay(&fpsManager)
+
+	// frame count should now be 1
+	if count, ok := GetFramecount(&fpsManager); !ok || count != 1 {
+		t.Errorf("gfx.Framecount() = (%v, %v) - want (1, true)", count, ok)
+	}
+}
