55356a2bddb0c6bcca8a004490d8b9edc0db3520
malashin <malashinanton@gmail.com>
2017-11-08 11:43:55 +1000

    sdl: events, haptic, helpers, hints... : Add documentation

--- sdl/joystick.go
+++ sdl/joystick.go
@@ -4,6 +4,8 @@ package sdl
 import "C"
 import "unsafe"
 
+// Hat positions.
+// (https://wiki.libsdl.org/SDL_JoystickGetHat)
 const (
 	HAT_CENTERED  = C.SDL_HAT_CENTERED
 	HAT_UP        = C.SDL_HAT_UP
@@ -16,8 +18,13 @@ const (
 	HAT_LEFTDOWN  = C.SDL_HAT_LEFTDOWN
 )
 
+// Joystick is an SDL joystick.
 type Joystick C.SDL_Joystick
+
+// JoystickGUID is a stable unique id for a joystick device.
 type JoystickGUID C.SDL_JoystickGUID
+
+// JoystickID is joystick's instance id.
 type JoystickID C.SDL_JoystickID
 
 func (joy *Joystick) cptr() *C.SDL_Joystick {
@@ -28,37 +35,44 @@ func (guid JoystickGUID) c() C.SDL_JoystickGUID {
 	return C.SDL_JoystickGUID(guid)
 }
 
-// NumJoysticks (https://wiki.libsdl.org/SDL_NumJoysticks)
+// NumJoysticks returns the number of joysticks attached to the system.
+// (https://wiki.libsdl.org/SDL_NumJoysticks)
 func NumJoysticks() int {
 	return (int)(C.SDL_NumJoysticks())
 }
 
-// JoystickNameForIndex (https://wiki.libsdl.org/SDL_JoystickNameForIndex)
+// JoystickNameForIndex returns the implementation dependent name of a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickNameForIndex)
 func JoystickNameForIndex(index int) string {
 	return (C.GoString)(C.SDL_JoystickNameForIndex(C.int(index)))
 }
 
-// JoystickOpen (https://wiki.libsdl.org/SDL_JoystickOpen)
+// JoystickOpen opens a joystick for use.
+// (https://wiki.libsdl.org/SDL_JoystickOpen)
 func JoystickOpen(index JoystickID) *Joystick {
 	return (*Joystick)(C.SDL_JoystickOpen(C.int(index)))
 }
 
-// JoystickName (https://wiki.libsdl.org/SDL_JoystickName)
+// Name returns the implementation dependent name of a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickName)
 func (joy *Joystick) Name() string {
 	return (C.GoString)(C.SDL_JoystickName(joy.cptr()))
 }
 
-// JoystickGetDeviceGUID (https://wiki.libsdl.org/SDL_JoystickGetDeviceGUID)
+// JoystickGetDeviceGUID returns the implementation dependent GUID for the joystick at a given device index.
+// (https://wiki.libsdl.org/SDL_JoystickGetDeviceGUID)
 func JoystickGetDeviceGUID(index int) JoystickGUID {
 	return (JoystickGUID)(C.SDL_JoystickGetDeviceGUID(C.int(index)))
 }
 
-// Joystick (https://wiki.libsdl.org/SDL_JoystickGetGUID)
+// GetGUID returns the implementation-dependent GUID for the joystick.
+// (https://wiki.libsdl.org/SDL_JoystickGetGUID)
 func (joy *Joystick) GetGUID() JoystickGUID {
 	return (JoystickGUID)(C.SDL_JoystickGetGUID(joy.cptr()))
 }
 
-// JoystickGetGUIDString (https://wiki.libsdl.org/SDL_JoystickGetGUIDString)
+// JoystickGetGUIDString returns an ASCII string representation for a given JoystickGUID.
+// (https://wiki.libsdl.org/SDL_JoystickGetGUIDString)
 func JoystickGetGUIDString(guid JoystickGUID) string {
 	_pszGUID := make([]rune, 1024)
 	pszGUID := C.CString(string(_pszGUID[:]))
@@ -67,76 +81,90 @@ func JoystickGetGUIDString(guid JoystickGUID) string {
 	return C.GoString(pszGUID)
 }
 
-// JoystickGetGUIDFromString (https://wiki.libsdl.org/SDL_JoystickGetGUIDFromString)
+// JoystickGetGUIDFromString converts a GUID string into a JoystickGUID structure.
+// (https://wiki.libsdl.org/SDL_JoystickGetGUIDFromString)
 func JoystickGetGUIDFromString(pchGUID string) JoystickGUID {
 	_pchGUID := C.CString(pchGUID)
 	defer C.free(unsafe.Pointer(_pchGUID))
 	return (JoystickGUID)(C.SDL_JoystickGetGUIDFromString(_pchGUID))
 }
 
-// JoystickGetAttached (https://wiki.libsdl.org/SDL_JoystickGetAttached)
+// GetAttached returns the status of a specified joystick.
+// (https://wiki.libsdl.org/SDL_JoystickGetAttached)
 func (joy *Joystick) GetAttached() bool {
 	return C.SDL_JoystickGetAttached(joy.cptr()) > 0
 }
 
-// JoystickInstanceID (https://wiki.libsdl.org/SDL_JoystickInstanceID)
+// InstanceID returns the instance ID of an opened joystick.
+// (https://wiki.libsdl.org/SDL_JoystickInstanceID)
 func (joy *Joystick) InstanceID() JoystickID {
 	return (JoystickID)(C.SDL_JoystickInstanceID(joy.cptr()))
 }
 
-// JoystickNumAxes (https://wiki.libsdl.org/SDL_JoystickNumAxes)
+// NumAxes returns the number of general axis controls on a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickNumAxes)
 func (joy *Joystick) NumAxes() int {
 	return (int)(C.SDL_JoystickNumAxes(joy.cptr()))
 }
 
-// JoystickNumBalls (https://wiki.libsdl.org/SDL_JoystickNumBalls)
+// NumBalls returns the number of trackballs on a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickNumBalls)
 func (joy *Joystick) NumBalls() int {
 	return (int)(C.SDL_JoystickNumBalls(joy.cptr()))
 }
 
-// JoystickNumHats (https://wiki.libsdl.org/SDL_JoystickNumHats)
+// NumHats returns the number of POV hats on a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickNumHats)
 func (joy *Joystick) NumHats() int {
 	return (int)(C.SDL_JoystickNumHats(joy.cptr()))
 }
 
-// JoystickNumButtons (https://wiki.libsdl.org/SDL_JoystickNumButtons)
+// NumButtons returns the number of buttons on a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickNumButtons)
 func (joy *Joystick) NumButtons() int {
 	return (int)(C.SDL_JoystickNumButtons(joy.cptr()))
 }
 
-// JoystickUpdate (https://wiki.libsdl.org/SDL_JoystickUpdate)
+// JoystickUpdate updates the current state of the open joysticks.
+// (https://wiki.libsdl.org/SDL_JoystickUpdate)
 func JoystickUpdate() {
 	C.SDL_JoystickUpdate()
 }
 
-// JoystickEventState (https://wiki.libsdl.org/SDL_JoystickEventState)
+// JoystickEventState enables or disables joystick event polling.
+// (https://wiki.libsdl.org/SDL_JoystickEventState)
 func JoystickEventState(state int) int {
 	return (int)(C.SDL_JoystickEventState(C.int(state)))
 }
 
-// JoystickGetAxis (https://wiki.libsdl.org/SDL_JoystickGetAxis)
+// GetAxis returns the current state of an axis control on a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickGetAxis)
 func (joy *Joystick) GetAxis(axis int) int16 {
 	return (int16)(C.SDL_JoystickGetAxis(joy.cptr(), C.int(axis)))
 }
 
-// JoystickGetHat (https://wiki.libsdl.org/SDL_JoystickGetHat)
+// GetHat returns the current state of a POV hat on a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickGetHat)
 func (joy *Joystick) GetHat(hat int) byte {
 	return (byte)(C.SDL_JoystickGetHat(joy.cptr(), C.int(hat)))
 }
 
-// JoystickGetBall (https://wiki.libsdl.org/SDL_JoystickGetBall)
+// GetBall returns the ball axis change since the last poll.
+// (https://wiki.libsdl.org/SDL_JoystickGetBall)
 func (joy *Joystick) GetBall(ball int, dx, dy *int) int {
 	_dx := (*C.int)(unsafe.Pointer(dx))
 	_dy := (*C.int)(unsafe.Pointer(dy))
 	return (int)(C.SDL_JoystickGetBall(joy.cptr(), C.int(ball), _dx, _dy))
 }
 
-// JoystickGetButton (https://wiki.libsdl.org/SDL_JoystickGetButton)
+// GetButton the current state of a button on a joystick.
+// (https://wiki.libsdl.org/SDL_JoystickGetButton)
 func (joy *Joystick) GetButton(button int) byte {
 	return (byte)(C.SDL_JoystickGetButton(joy.cptr(), C.int(button)))
 }
 
-// JoystickClose (https://wiki.libsdl.org/SDL_JoystickClose)
+// Close closes a joystick previously opened with JoystickOpen().
+// (https://wiki.libsdl.org/SDL_JoystickClose)
 func (joy *Joystick) Close() {
 	C.SDL_JoystickClose(joy.cptr())
 }
--- sdl/keyboard.go
+++ sdl/keyboard.go
@@ -5,20 +5,23 @@ import "C"
 import "unsafe"
 import "reflect"
 
-// Keysym (https://wiki.libsdl.org/SDL_Keysym)
+// Keysym contains key information used in key events.
+// (https://wiki.libsdl.org/SDL_Keysym)
 type Keysym struct {
-	Scancode Scancode
-	Sym      Keycode
-	Mod      uint16
-	Unicode  uint32
+	Scancode Scancode // SDL physical key code
+	Sym      Keycode  // SDL virtual key code
+	Mod      uint16   // current key modifiers
+	unused   uint32   // unused
 }
 
-// GetKeyboardFocus (https://wiki.libsdl.org/SDL_GetKeyboardFocus)
+// GetKeyboardFocus returns the window which currently has keyboard focus.
+// (https://wiki.libsdl.org/SDL_GetKeyboardFocus)
 func GetKeyboardFocus() *Window {
 	return (*Window)(unsafe.Pointer(C.SDL_GetKeyboardFocus()))
 }
 
-// GetKeyboardState (https://wiki.libsdl.org/SDL_GetKeyboardState)
+// GetKeyboardState returns a snapshot of the current state of the keyboard.
+// (https://wiki.libsdl.org/SDL_GetKeyboardState)
 func GetKeyboardState() []uint8 {
 	var numkeys C.int
 	start := C.SDL_GetKeyboardState(&numkeys)
@@ -29,76 +32,90 @@ func GetKeyboardState() []uint8 {
 	return *(*[]uint8)(unsafe.Pointer(&sh))
 }
 
-// GetModState (https://wiki.libsdl.org/SDL_GetModState)
+// GetModState returns the current key modifier state for the keyboard.
+// (https://wiki.libsdl.org/SDL_GetModState)
 func GetModState() Keymod {
 	return (Keymod)(C.SDL_GetModState())
 }
 
-// SetModState (https://wiki.libsdl.org/SDL_SetModState)
+// SetModState sets the current key modifier state for the keyboard.
+// (https://wiki.libsdl.org/SDL_SetModState)
 func SetModState(mod Keymod) {
 	C.SDL_SetModState(mod.c())
 }
 
-// GetKeyFromScancode (https://wiki.libsdl.org/SDL_GetKeyFromScancode)
+// GetKeyFromScancode returns the key code corresponding to the given scancode according to the current keyboard layout.
+// (https://wiki.libsdl.org/SDL_GetKeyFromScancode)
 func GetKeyFromScancode(code Scancode) Keycode {
 	return (Keycode)(C.SDL_GetKeyFromScancode(code.c()))
 }
 
-// GetScancodeFromKey (https://wiki.libsdl.org/SDL_GetScancodeFromKey)
+// GetScancodeFromKey returns the scancode corresponding to the given key code according to the current keyboard layout.
+// (https://wiki.libsdl.org/SDL_GetScancodeFromKey)
 func GetScancodeFromKey(code Keycode) Scancode {
 	return (Scancode)(C.SDL_GetScancodeFromKey(code.c()))
 }
 
-// GetScancodeName (https://wiki.libsdl.org/SDL_GetScancodeName)
+// GetScancodeName returns a human-readable name for a scancode
+// (https://wiki.libsdl.org/SDL_GetScancodeName)
 func GetScancodeName(code Scancode) string {
 	return (C.GoString)(C.SDL_GetScancodeName(code.c()))
 }
 
-// GetScancodeFromName (https://wiki.libsdl.org/SDL_GetScancodeFromName)
+// GetScancodeFromName returns a scancode from a human-readable name.
+// (https://wiki.libsdl.org/SDL_GetScancodeFromName)
 func GetScancodeFromName(name string) Scancode {
 	_name := C.CString(name)
 	defer C.free(unsafe.Pointer(_name))
 	return (Scancode)(C.SDL_GetScancodeFromName(_name))
 }
 
-// GetKeyName (https://wiki.libsdl.org/SDL_GetKeyName)
+// GetKeyName returns a human-readable name for a key.
+// (https://wiki.libsdl.org/SDL_GetKeyName)
 func GetKeyName(code Keycode) string {
 	return (C.GoString)(C.SDL_GetKeyName(code.c()))
 }
 
-// GetKeyFromName (https://wiki.libsdl.org/SDL_GetKeyFromName)
+// GetKeyFromName returns a key code from a human-readable name.
+// (https://wiki.libsdl.org/SDL_GetKeyFromName)
 func GetKeyFromName(name string) Keycode {
 	_name := C.CString(name)
 	defer C.free(unsafe.Pointer(_name))
 	return (Keycode)(C.SDL_GetKeyFromName(_name))
 }
 
-// StartTextInput (https://wiki.libsdl.org/SDL_StartTextInput)
+// StartTextInput starts accepting Unicode text input events.
+// (https://wiki.libsdl.org/SDL_StartTextInput)
 func StartTextInput() {
 	C.SDL_StartTextInput()
 }
 
-// IsTextInputActive (https://wiki.libsdl.org/SDL_IsTextInputActive)
+// IsTextInputActive checks whether or not Unicode text input events are enabled.
+// (https://wiki.libsdl.org/SDL_IsTextInputActive)
 func IsTextInputActive() bool {
 	return C.SDL_IsTextInputActive() > 0
 }
 
-// StopTextInput (https://wiki.libsdl.org/SDL_StopTextInput)
+// StopTextInput stops receiving any text input events.
+// (https://wiki.libsdl.org/SDL_StopTextInput)
 func StopTextInput() {
 	C.SDL_StopTextInput()
 }
 
-// SetTextInputRect (https://wiki.libsdl.org/SDL_SetTextInputRect)
+// SetTextInputRect sets the rectangle used to type Unicode text inputs.
+// (https://wiki.libsdl.org/SDL_SetTextInputRect)
 func SetTextInputRect(rect *Rect) {
 	C.SDL_SetTextInputRect(rect.cptr())
 }
 
-// HasScreenKeyboardSupport (https://wiki.libsdl.org/SDL_HasScreenKeyboardSupport)
+// HasScreenKeyboardSupport reports whether the platform has some screen keyboard support.
+// (https://wiki.libsdl.org/SDL_HasScreenKeyboardSupport)
 func HasScreenKeyboardSupport() bool {
 	return C.SDL_HasScreenKeyboardSupport() > 0
 }
 
-// IsScreenKeyboardShown (https://wiki.libsdl.org/SDL_IsScreenKeyboardShown)
+// IsScreenKeyboardShown reports whether the screen keyboard is shown for given window.
+// (https://wiki.libsdl.org/SDL_IsScreenKeyboardShown)
 func IsScreenKeyboardShown(window *Window) bool {
 	return C.SDL_IsScreenKeyboardShown(window.cptr()) > 0
 }
