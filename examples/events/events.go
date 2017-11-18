247fc69773bbc7096fca742c0ee10f7f2e1f4f80
Lilis Iskandar <lilis@veand.co>
2017-11-18 23:38:29 +0800

    examples: events: events.go: Update to work with new API
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

--- examples/events/events.go
+++ examples/events/events.go
@@ -46,10 +46,7 @@ func run() int {
 			case *sdl.MouseWheelEvent:
 				fmt.Printf("[%d ms] MouseWheel\ttype:%d\tid:%d\tx:%d\ty:%d\n",
 					t.Timestamp, t.Type, t.Which, t.X, t.Y)
-			case *sdl.KeyDownEvent:
-				fmt.Printf("[%d ms] Keyboard\ttype:%d\tsym:%c\tmodifiers:%d\tstate:%d\trepeat:%d\n",
-					t.Timestamp, t.Type, t.Keysym.Sym, t.Keysym.Mod, t.State, t.Repeat)
-			case *sdl.KeyUpEvent:
+			case *sdl.KeyboardEvent:
 				fmt.Printf("[%d ms] Keyboard\ttype:%d\tsym:%c\tmodifiers:%d\tstate:%d\trepeat:%d\n",
 					t.Timestamp, t.Type, t.Keysym.Sym, t.Keysym.Mod, t.State, t.Repeat)
 			case *sdl.JoyAxisEvent:
