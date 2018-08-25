2e2b1e5e49c26eceda0535b3c5e3ff5133fc93a6
jclc <jclc@protonmail.com>
2018-08-25 06:21:33 +0300

    sdl: add missing vulkan macro when < v2.0.6

--- sdl/vulkan.go
+++ sdl/vulkan.go
@@ -12,6 +12,12 @@ package sdl
 #else
 
 #define VK_DEFINE_HANDLE(object) typedef struct object##_T* object;
+#if defined(__LP64__) || defined(_WIN64) || defined(__x86_64__) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
+#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef struct object##_T *object;
+#else
+#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef uint64_t object;
+#endif
+
 VK_DEFINE_HANDLE(VkInstance)
 VK_DEFINE_NON_DISPATCHABLE_HANDLE(VkSurfaceKHR)
 typedef VkInstance SDL_vulkanInstance;
