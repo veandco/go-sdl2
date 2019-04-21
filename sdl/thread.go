ac6422e4f04824a94b843fd5360e2bbca88a99f7
Lilis Iskandar <lilis@veand.co>
2019-04-21 15:05:52 +0800

    sdl: add binding for SDL_ThreadID() as sdl.CurrentThreadID()
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

---
+++ sdl/thread.go
@@ -0,0 +1,10 @@
+package sdl
+
+// #include "sdl_wrapper.h"
+import "C"
+
+// CurrentThreadID gets the thread identifier for the current thread. 
+// (https://wiki.libsdl.org/SDL_ThreadID)
+func CurrentThreadID() uint {
+	return uint(C.SDL_ThreadID())
+}
