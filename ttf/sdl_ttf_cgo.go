0be71610627d7ebca922db6b857f00642eb66f45
Troels Henriksen <athas@sigkill.dk>
2019-06-12 19:42:59 +0200

    sdl2_ttf: use correct pkg-config package.

--- ttf/sdl_ttf_cgo.go
+++ ttf/sdl_ttf_cgo.go
@@ -3,6 +3,6 @@
 package ttf
 
 //#cgo windows LDFLAGS: -lSDL2 -lSDL2_ttf
-//#cgo linux freebsd darwin pkg-config: sdl2
+//#cgo linux freebsd darwin pkg-config: SDL2_ttf
 //#cgo linux freebsd darwin LDFLAGS: -lSDL2_ttf
 import "C"
