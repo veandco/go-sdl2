108ccb6c3a6ef8e4680c7017017fad3f74e20ad0
Lilis Iskandar <lilis@veand.co>
2020-01-18 13:06:18 +0800

    sdl/system: add SDL_IsTablet for 2.0.9
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

---
+++ sdl/system.go
@@ -0,0 +1,27 @@
+package sdl
+
+/*
+#include "sdl_wrapper.h"
+
+#if !(SDL_VERSION_ATLEAST(2,0,9))
+
+#if defined(WARN_OUTDATED)
+#pragma message("SDL_IsTablet is not supported before SDL 2.0.9")
+#endif
+
+static inline SDL_bool SDL_IsTablet()
+{
+	return SDL_FALSE;
+}
+
+#endif
+
+*/
+import "C"
+
+
+// IsTablet returns true if the current device is a tablet
+// TODO: (https://wiki.libsdl.org/SDL_IsTablet)
+func IsTablet() bool {
+	return C.SDL_IsTablet() == C.SDL_TRUE
+}
