6541014ee79bc1e4c20370b378e03384b22f52f8
Keithcat1 <47483928+Keithcat1@users.noreply.github.com>
2021-08-21 22:26:20 -0500

    Don't use -mwindows flag when using static linking on Windows (#491)
    * static: remove -Wl,--no-undefined from ldflags. Go 1.17 will have support for linking with LLVM lld, but LLD its self doesn't accept --no-undefined
    
    * Don't use -mwindows flag when using static linking on Windows
    
    The -mwindows flag tells GCC that we want the binary to be a GUI binary and have no command line termina, but there are two reasons it's a problem.
    First, normal MinGW ignores it, for example winlibs.com. LLVM-MinGW will properly apply this flag though.
    Second, it forces you to stop printing stuff to the console, and if you don't have a panic handler, it silently swallows the panic message and just exits with no indication of what went wrong. Also, Go has its own flag for making GUI binaries "go build --ldflags=-h WINDOWSGUI" has the same effect.

--- sdl/sdl_cgo_static.go
+++ sdl/sdl_cgo_static.go
@@ -6,8 +6,8 @@ package sdl
 //#cgo LDFLAGS: -L${SRCDIR}/../.go-sdl2-libs
 //#cgo linux,386 LDFLAGS: -lSDL2_linux_386 -lm -ldl -lasound -lm -ldl -lpthread -lX11 -lXext -lXcursor -lXinerama -lXi -lXrandr -lXss -lXxf86vm -lpthread -lrt
 //#cgo linux,amd64 LDFLAGS: -lSDL2_linux_amd64 -lm -ldl -lasound -lm -ldl -lpthread -lX11 -lXext -lXcursor -lXinerama -lXi -lXrandr -lXss -lXxf86vm -lpthread -lrt
-//#cgo windows,386 LDFLAGS: -lSDL2_windows_386 -lSDL2main_windows_386 -mwindows -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid -static-libgcc
-//#cgo windows,amd64 LDFLAGS: -lSDL2_windows_amd64 -lSDL2main_windows_amd64 -mwindows -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lversion -luuid -lsetupapi -static-libgcc
+//#cgo windows,386 LDFLAGS: -lSDL2_windows_386 -lSDL2main_windows_386 -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid -static-libgcc
+//#cgo windows,amd64 LDFLAGS: -lSDL2_windows_amd64 -lSDL2main_windows_amd64 -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lversion -luuid -lsetupapi -static-libgcc
 //#cgo darwin,amd64 LDFLAGS: -lSDL2_darwin_amd64 -lm -liconv -Wl,-framework,OpenGL -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-framework,Metal
 //#cgo android,arm LDFLAGS: -lSDL2_android_arm -lm -ldl -llog -landroid -lGLESv2 -lGLESv1_CM
 //#cgo linux,arm,!android LDFLAGS: -L/opt/vc/lib -L/opt/vc/lib64 -lSDL2_linux_arm -lm -ldl -liconv -lbcm_host -lvcos -lvchiq_arm -pthread
