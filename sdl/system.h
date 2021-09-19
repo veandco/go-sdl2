fb0dfc6d028db014b1e89dd319bdc9bab04d6d21
Lilis Iskandar <lilis@veand.co>
2021-09-19 20:29:33 +0800

    sdl/system: Fix Windows-specific bindings to SDL2 2.0.16
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

--- sdl/system.h
+++ sdl/system.h
@@ -2,8 +2,6 @@
 #define _GO_SDL_SYSTEM_H
 
 #if defined(_WIN32)
-	#include <SDL2/SDL_system.h>
-
 	extern void SetWindowsMessageHook();
 #endif
 
--- sdl/system_windows.go
+++ sdl/system_windows.go
@@ -1,19 +1,16 @@
 package sdl
 
 /*
+#include "sdl_wrapper.h"
 #include "system.h"
 
 #if !(SDL_VERSION_ATLEAST(2,0,16))
 
 #if defined(WARN_OUTDATED)
-#pragma message("SDL_SetWindowsMessageHook is not supported before SDL 2.0.16")
 #pragma message("SDL_RenderGetD3D11Device is not supported before SDL 2.0.16")
 #endif
 
-static void SDL_SetWindowsMessageHook(SDL_WindowsMessageHook callback, void *userdata)
-{
-	return
-}
+typedef struct ID3D11Device ID3D11Device;
 
 static ID3D11Device* SDL_RenderGetD3D11Device(SDL_Renderer * renderer)
 {
@@ -50,7 +47,7 @@ func goWindowsMessageHook(userdata interface{}, hWnd unsafe.Pointer, message uin
 // SDL_RenderGetD3D11Device gets the D3D11 device associated with a renderer.
 // (https://wiki.libsdl.org/SDL_RenderGetD3D11Device)
 func (renderer *Renderer) GetD3D11Device() (device *ID3D11Device, err error) {
-	device = C.SDL_RenderGetD3D11Device(renderer.cptr())
+	device = (*ID3D11Device)(C.SDL_RenderGetD3D11Device(renderer.cptr()))
 	if device == nil {
 		err = GetError()
 	}
