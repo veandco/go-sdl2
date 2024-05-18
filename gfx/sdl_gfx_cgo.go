d9801e0db990b3aacb6386d0e6d29e3890f3c10f
Lilis Iskandar <lilis@veand.co>
2024-05-18 12:32:29 +0800

    gfx: Update pkg-config to use sdl2_gfx instead of sdl2
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

--- gfx/sdl_gfx_cgo.go
+++ gfx/sdl_gfx_cgo.go
@@ -3,6 +3,6 @@
 package gfx
 
 //#cgo windows LDFLAGS: -lSDL2 -lSDL2_gfx
-//#cgo linux freebsd darwin pkg-config: sdl2
+//#cgo linux freebsd darwin pkg-config: sdl2_gfx
 //#cgo linux freebsd darwin LDFLAGS: -lSDL2_gfx
 import "C"
